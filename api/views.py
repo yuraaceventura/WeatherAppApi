dc36f9392d7998fb24977d6bd89db415f1f04f3b
yuraaceventura <yuraaceventura@gmail.com>
2025-05-26 13:46:08 +0300

    Minor fixes

--- api/views.py
+++ api/views.py
@@ -16,18 +16,19 @@ router = APIRouter()
 api_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 
 
-
 @router.post("/weather/{city}")
 async def get_weather(city: str):
-    if response := r.hmget(city, keys=["temp", "humidity"]) :
-        if response[0] is not None:
-            return response
+    cached_response = r.hmget(city, keys=["temperature", "humidity"])
+    if all(cached_response) and cached_response[0] is not None:
+        return {"temperature": cached_response[0], "humidity": cached_response[1]}
+
     request_url = api_url + city + "?key=" + settings.api.KEY
-    response = requests.get(request_url)
-    response = response.json()
+    response = requests.get(request_url).json()
     response = response["days"][0]
     temp = response["temp"]
     humidity = response["humidity"]
-    r.hset(city, mapping={"temp": f"temperature: {str(temp)}", "humidity": f"humidity:{str(humidity)}"})
+
+    r.hset(city, mapping={"temperature": f"{temp}", "humidity": f"{humidity}"})
     r.expire(city, 60)
-    return r.hmget(city, keys=["temp", "humidity"])
+    return {"temperature": f"{temp}", "humidity": f"{humidity}"}
+
